e98162794b71698c9203197381c77442160bf8a1
Miles Cremen <19142852@student.curtin.edu.au>
2021-05-07 22:46:59 +1000

    Create css for Mobile Navigation

---
+++ src/components/InputCard/InputCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './InputCard.css';
+
+function InputCard() {
+    return (
+        <div>
+            <div className="orig-link"><a href="/" alt="">https://google.com</a></div>
+            <div className="shortend-link"><a href="/" alt="">http://reLink//k41lkfe</a></div>
+            <button>Copy</button>
+        </div>
+    )
+}
+
+export default InputCard
