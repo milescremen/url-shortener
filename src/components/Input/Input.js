4842174d1303b2652f281cf38a90509dece6174e
Miles Cremen <19142852@student.curtin.edu.au>
2021-05-20 13:43:11 +0800

    Implemented shortcode api call

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Input.css';
 import LinkCard from '../LinkCard/LinkCard.js';
+const axios = require('axios').default;
 
 function Input() {
 
-    let links = [
-            { id: 1, origLink: 'www.facebook.com', shortLink: "hehe" }, 
-            { id: 2, origLink: 'www.google.com', shortLink: "test"}
+    const linksList = [
+            
         ];
+
+    const [text, setText] = useState('');
+    const [links, setLinks] = useState(linksList);
+
+    const addLink = async (e) => {
+        e.preventDefault();
+
+        try {
+            const url = text;
+            setText('');
+            
+            const res = await axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`);
+
+            const newLink = {
+                id: 1,
+                origLink: text,
+                shortLink: res.data.result.full_share_link
+            };
+
+            setLinks([...links, newLink]);
+        } catch(err) {
+            console.error(err);
+        }
+    }
 
     return (
         <div className="input-container">
             <div className="input">
-                <input type="text" placeholder="Shorten a link here..."/> 
+                <input type="text" value={text} onChange={(e) => {setText(e.target.value)}} placeholder="Shorten a link here..."/> 
 
-                <button>    
+                <button onClick={addLink}>    
                     Shorten It!
                 </button>
             </div>
