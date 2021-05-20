47df0ce08820da33e2015e11d7828f11feaf7b98
Miles Cremen <19142852@student.curtin.edu.au>
2021-05-20 12:43:20 +0800

    Navigation Toggle, no animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from './components/Header/Header.js';
-import Navigation from './components/Navigation/Navigation.js';
 import Landing from './components/Landing/Landing.js';
 import Input from './components/Input/Input.js';
 import Main from './components/Main/Main.js';
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Header />
 
-      <Navigation />
 
       <Landing />
       <Input />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../images/logo.svg';
 import MenuIcon from '@material-ui/icons/Menu';
+import Navigation from '../Navigation/Navigation.js';
 import './Header.css'
 
 
+
 function Header() {
+
+    const [toggleNav, setToggleNav] = useState('false');
+
     return (
-        <div className="header">
-            <img src={Logo} alt="Shortly"/>
+        <div className="header-container">
+            <div className="header">
+                <img src={Logo} alt="Shortly"/>
 
-            <MenuIcon className="hamburger" fontSize="large"/>
+                <MenuIcon onClick={() => { setToggleNav(!toggleNav)}} className="hamburger" fontSize="large"/>
 
-            {
-                /*
+                {
+                    /*
 
-                <a href="">Features</a>      
-                <a href="">Pricing</a>      
-                <a href="">Resources</a>      
+                    <a href="">Features</a>      
+                    <a href="">Pricing</a>      
+                    <a href="">Resources</a>      
 
-                <button>Login</button>
-                <button>Sign Up</button>
-                */
-            }
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                    */
+                }
+            </div>
 
+            { toggleNav && <Navigation />  }
         </div>
     )
 }
