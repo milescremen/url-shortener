d9214f1616677099279a3ee2804f7c54558571ab
Miles Cremen <19142852@student.curtin.edu.au>
2021-05-28 22:41:22 +0800

    Implemented copying to clipboard and fixed links

--- src/components/LinkCard/LinkCard.js
+++ src/components/LinkCard/LinkCard.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LinkCard.css';
 
 function LinkCard({ link }) {
+
+    const [copied, setCopied] = useState()
+
+    const copyLink = () => {
+        navigator.clipboard.writeText(link.shortLink);
+        setCopied(true);
+    }
+
     return (
         <div className="link-card">
-            <div className="orig-link"><a href="/" alt="">{link.origLink}</a></div>
-            <div className="short-link"><a href="/" alt="">{link.shortLink}</a></div>
+            <div className="orig-link"><a href={"https://" + link.origLink} target="_blank" rel="noopener noreferrer" alt="">{link.origLink}</a></div>
+            <div className="short-link"><a href={"https://" + link.shortLink} target="_blank" rel="noopener noreferrer" alt="">{link.shortLink}</a></div>
             
-            <button>Copy</button>
+            <button onClick={copyLink}>
+                { !copied ? 'Copy' : 'Copied!' }
+            </button>
         </div>
     )
 }
